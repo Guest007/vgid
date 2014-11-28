3b0ff5639c7092a9b9a5fafbd627fb71ce63dab6
Guest007 <trash4@yandex.ru>
2014-11-28 18:20:38 +0300

    first steps

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import ugettext, ugettext_lazy as _
 
-# Create your models here.
+
+class ParentModel(models.Model):
+    """
+    Abstract parent for all main records
+    """
+    title = models.CharField(_("title"), max_length=500, default="")
+    slug = models.SlugField(_("slug"))
+    created = models.DateTimeField(auto_now_add=True)
+    changed = models.DateTimeField(auto_now=True)
+    is_publish = models.BooleanField(_('Is Published'), default=False)
+    description = models.TextField(_("Description"), blank=True, null=True)
+    text = models.TextField(_("Text"), blank=True, null=True)
+    # image = models.ImageField(_("Image"))
+    gallery = models.ForeignKey("Gallery", blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+    def __unicode__(self):
+        return self.title
+
+
+class Gallery(models.Model):
+    """
+    Main gallery for all apps
+    """
+    image = models.ImageField(_("Image"))
+    created = models.DateTimeField(auto_now_add=True)
+    changed = models.DateTimeField(auto_now=True)
--- apps/events/models.py
+++ apps/events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core.models import ParentModel
+from django.utils.translation import ugettext, ugettext_lazy as _
 
-# Create your models here.
+
+class Event(ParentModel):
+    """
+    main Event model for current news and for past events
+    """
+    image = models.ImageField(_("Image"), upload_to='enents')
+
+
+class Section(models.Model):
+    pass
