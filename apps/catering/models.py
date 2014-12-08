9951989c88aadf0167190dc7dd84ae4e48bd32ee
Guest007 <trash4@yandex.ru>
2014-12-08 14:51:15 +0300

    catering

--- apps/catering/models.py
+++ apps/catering/models.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from core.models import (ParentModel, SimpleAbstract, get_file_name)
+from django.utils.translation import ugettext, ugettext_lazy as _
+from sorl.thumbnail import ImageField
 
-# Create your models here.
+
+class CatType(SimpleAbstract):
+    """
+        Тип заведения
+    """
+    pass
+
+
+class Catering(ParentModel):
+    """
+    Питание
+    """
+    type_of_catering = models.ForeignKey(CatType, help_text=u'Тип заведения')
+    image = ImageField(_("Image"), upload_to=get_file_name, blank=True,
+                       null=True)
+    address = models.CharField(u'Адрес', max_length=200)
+
