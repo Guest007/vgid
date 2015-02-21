5e49d81ec6f168233abf6fcc105c27d684d691dc
Guest007 <trash4@yandex.ru>
2015-02-21 08:32:40 +0300

    updates, side_menu, begin with templates

---
+++ apps/core/templatetags/core_utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = 'guest007'
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('core/tag.side_menu.html', takes_context=True)
+def side_menu(context):
+    request = context['request']
+    path = request.path.lower()
+
+    return True
