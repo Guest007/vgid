09cbb65f3afcbdd830571376cf4f62fc906c8a3b
Guest007 <trash4@yandex.ru>
2015-03-11 21:33:57 +0300

    filters can be off, slider, other fixes

--- apps/showplaces/views.py
+++ apps/showplaces/views.py
@@ -18,21 +18,23 @@ class ShowPlacesList(AjaxListView):
         dist = self.request.GET.get('dist')
         p_type = self.request.GET.get('pt')
         if dist:
-            if dist == "1":
-                qs = qs.filter(distance__lt=1.00)
-            elif dist == "2":
-                qs = qs.filter(distance__gt=1.00, distance__lt=10.01)
-            elif dist == "3":
-                qs = qs.filter(distance__gt=10.00, distance__lt=30.01)
-            elif dist == "4":
-                qs = qs.filter(distance__gt=30.00, distance__lt=50.01)
-            elif dist == "5":
-                qs = qs.filter(distance__gt=50.00, distance__lt=100.01)
-            elif dist == "6":
-                qs = qs.filter(distance__gt=100.00)
+            if '-' not in dist:
+                if dist == "1":
+                    qs = qs.filter(distance__lt=1.00)
+                elif dist == "2":
+                    qs = qs.filter(distance__gt=1.00, distance__lt=10.01)
+                elif dist == "3":
+                    qs = qs.filter(distance__gt=10.00, distance__lt=30.01)
+                elif dist == "4":
+                    qs = qs.filter(distance__gt=30.00, distance__lt=50.01)
+                elif dist == "5":
+                    qs = qs.filter(distance__gt=50.00, distance__lt=100.01)
+                elif dist == "6":
+                    qs = qs.filter(distance__gt=100.00)
 
         if p_type:
-            qs = qs.filter(place_type=p_type)
+            if '-' not in p_type:
+                qs = qs.filter(place_type=p_type)
 
         return qs
 
@@ -42,10 +44,12 @@ class ShowPlacesList(AjaxListView):
 
         dist = self.request.GET.get('dist')
         if dist:
-            context['dist'] = dist
+            if '-' not in dist:
+                context['dist'] = dist
 
         pt = self.request.GET.get('pt')
         if pt:
-            context['pt_active'] = PlaceType.objects.get(pk=pt)
+            if '-' not in pt:
+                context['pt_active'] = PlaceType.objects.get(pk=pt)
 
         return context
